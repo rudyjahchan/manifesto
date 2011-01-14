962c8985be5022f3ee5a0b1c0420a74e3701bbc4
Rudy Jahchan <rudy@rudyjahchan.com>
2011-01-13 22:24:25 -0800

    Fixed search

--- searchform.php
+++ searchform.php
@@ -1,4 +1,4 @@
 <form action="<?php bloginfo('url'); ?>" id="searchform" method="get" role="search">
-	<input type="text" id="s" name="s" value="">
-	<input type="submit" value="Search" id="searchsubmit">
+	<input type="text" id="s" name="s" value="" />
+	<input type="submit" value="Search" id="searchsubmit" />
 </form>
