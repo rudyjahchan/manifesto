1c87f186db94edc955cac83a8f49836e4fcbfaf6
Rudy Jahchan <rudy@rudyjahchan.com>
2011-01-14 21:13:40 -0800

    Add credits and feed links

--- footer.php
+++ footer.php
@@ -20,7 +20,23 @@
 	?>
 	</ul>
 </nav>
-
+<div class="clearfix">
+			<div id="site-info">
+				<a href="<?php echo home_url( '/' ) ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home">
+					<?php bloginfo( 'name' ); ?>
+				</a>,
+				<a href="<?php echo esc_url( __('http://wordpress.org/', 'twentyten') ); ?>"
+						title="<?php esc_attr_e('Semantic Personal Publishing Platform', 'twentyten'); ?>" rel="generator">
+					<?php printf( __('Proudly powered by %s.', 'twentyten'), 'WordPress' ); ?>
+				</a>
+			</div>
+			
+			<div id="site-feeds">
+			Subscribe:
+				<a href="<?php bloginfo('rss2_url'); ?>">Entries </a> |
+				<a href="<?php bloginfo('comments_rss2_url'); ?>">Comments </a>			
+		</div>
+</div>
 <?php
 	if (function_exists('bccl_full_html_license')) {
 	?>
